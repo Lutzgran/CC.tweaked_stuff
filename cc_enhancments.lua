eecffcfcf75c7a0b8783a2365476a27f6d514592
Lutzgran <157606898+Lutzgran@users.noreply.github.com>
2024-05-19 13:39:32 +0100

    Update cc_enhancments.lua

--- cc_enhancments.lua
+++ cc_enhancments.lua
@@ -1,6 +1,6 @@
 
 #uses the first letter of a direction and the distance it will travle
 
-local function EnhancedTurn(dirrection_letter, distance)
-
-  turtle.
+local function EnhancedTurn(direction_letter, distance)
+  
+  if direction
