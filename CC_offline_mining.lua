0977146a199b7909bbd58ba1b1df1ed11064f73b
Lutzgran <157606898+Lutzgran@users.noreply.github.com>
2024-08-07 13:27:26 +0200

    Update and rename Self starting mining script to CC_offline_mining.lua

---
+++ CC_offline_mining.lua
@@ -0,0 +1,54 @@
+
+
+   # needed to make a mining script that didnt mess up just because the chunk got unloaded or the server restarted
+   # first make a folder for the settings
+
+
+
+   
+local depth = 0
+local xPos,zPos = 0,0
+local xDir,zDir = 0,1
+
+local goTo
+local refuel
+
+
+
+
+
+function trunRight(amou
+   
+   
+
+Local refuel(LowFuel,returned)
+
+   if lowFuel then
+      goTo(0,0,0,0,0)
+      local Fuel = true
+   elseif returned then
+      local Fuel = true
+
+   if Fuel then
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function offline_mining(area)
+
+   
+
+
+
