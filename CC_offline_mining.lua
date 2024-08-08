cf2de81828a637495346f3dc5911739ff378bc19
Lutzgran <157606898+Lutzgran@users.noreply.github.com>
2024-08-08 22:12:27 +0100

    Update CC_offline_mining.lua

--- CC_offline_mining.lua
+++ CC_offline_mining.lua
@@ -1,83 +1,124 @@
    --needed to make a mining script that didnt mess up just because the chunk got unloaded or the server restarted
    --first make a folder for the settings
 
-local area = 0
-depth = 0
-d = 0
-xPos,zPos = 0,0
-local current_pos = { xPos = 0, depth = 0, zPos = 0, dir = 0 }
-local saved_pos   = { xPos = 0, depth = 0, zPos = 0, dir = 0 }
-local home_pos    = { xPos = 0, depth = 0, zPos = 0, dir = 0 }
 
-local goToPos
-local Fuel
-local fuel_amount 
+local current_pos = { xPos = 0, y = 0, zPos = 0, dir = 0 }
+local saved_pos   = { xPos = 0, y = 0, zPos = 0, dir = 0 }
+local home_pos    = { xPos = 0, y = 0, zPos = 0, dir = 0 }
+
+homeAmount = (saved_pos[1] + saved_pos[2] + saved_pos[3])*2
 
 
-function excavateBetter(size,height)
-   local isFinished = False
-   local usehHeight = height ~= nil and heaight > 0
-   local heightCount = 0
-
-   if useHeight then
-      if startNextLevel() then
-         heightCount = heightCount + 1
-         isFinished = height >= heightCount
-      end
-   else
-      isFinisshed = startNextLevel()
-   end
-
-    while isFinished do
-        for i = 0,size -1 do
-            for j = 0,size-1 do
-                dig(f)
-                go(f)
-            end
-            if size ~= 
-               if i % 2 == 0 then
-                   trun(r)
-                   dig(f)
-                   go(f)
-                   trun(r)
-               else
-                   trun(l)
-                   dig(f)
-                   go(f)
-                   trun(l)
-               end
-            end  
-        end
-        nilBLevel = dig("d") and go("d")
-        if size % 2 == 1 then
-            turn("r")
-            turn("r")
-        else
-            turn("r")
-        end
-        if useHeight then
-            if startNexyLevel() then
-            heightCount = heightCount + 1
-            isFinished = heaightCount >= heightCount
-            end
-         else
-            isFinisshed = startNextLevel()
-         end    
-         
-        nilBLevel = dig("d") and go("d")
-   end
-   
-   local function startNextlevel()
-      local atNext = go("d")
-      if not atNext then
-         dig("d")
-         atNext = go("d")
-      end
-   end 
-   return atNext
+function getPos()
+    local xPos, depth, zPos = gps.locate()
+    local coords = {xPos ,depth ,zPos}
+    return coords
 end
 
+function updateCoords(pos)
 
+    
+    
+end
+   
+function excavateBetter(size)
+    local nilBLevel = dig("d") and go("d")
+    
+    if restart then
+        goToMiningSite()
+        restart = false
+    end
+    while nilBLevel do
+        for i = 1,size do
+            for j = 1,size do
+                dig(d)
+                go(g)
+            end
+            if size ~= - 1 then
+                if i % 2 == 0 then
+                    trun(r)
+                    dig(f)
+                    go(f)
+                    trun(r)
+                else
+                    trun(l)
+                    dig(f)
+                    go(f)
+                    trun(l)
+                end
+            end
+            current_pos  = saved_pos
+            if inventoryCheck or fuelCheck then
+                goOfLoad()
+                reFuel()
+            end
+        end 
+           if size % 2 == 1 then
+               turn("r")
+               turn("r")
+           else
+               turn("r")
+           end
+    end   
+end
+
+function saveData()
+
+    if not fs.exists('/data') then
+        fs.makeDir('/data')
+    end
+    
+end
+   
+function inventoryCheck()  
+    local full = false
+
+    if turtle.getItemCount(turtle.select(16)) == 0 then
+        full = true
+    end
+    return full
+end
+
+function fuelCheck(size)  
+    local enoughFuel = false
+    local fuelAmount = homeAmount + ((size*size)*5)
+       
+    if getFuelLevel() >= fuelAmount then
+        enoughFuel = true
+    end
+    return enoughFuel
+end
+
+function goOfLoad()
+
+    goToPos(home_pos)
+    rotate(0,2)
+    for i = 1,16,1 do
+        turtle.select(i)
+        turtle.drop()
+    end
+    rotate(2,0)
+
+end
+
+function reFuel()
+
+    local success = false
+    goToPos(home_pos)
+    rotate(0,3)
+    turtle.select(1)
+    turtle.suck()
+    turtle.refuel()
+    rotate(3,0)
+
+    if fuelCheck == false then
+        print("no more fuel!")
+    else
+        success = true
+    end
+    return success
+end
+   
 function dig(str)
     local success = false
     if string.lower(str) == "forward" or string.lower(str)  == "f" then success = turtle.dig()
@@ -92,7 +133,7 @@ function go(str)
     local success = false
     if string.lower(str) == "down" or string.lower(str) == "d" then
         if turtle.down() then
-            current_pos["depth"] = current_pos["depth"] - 1
+            current_pos["y"] = current_pos["y"] - 1
             success = true
         else
             term.write("I'm stuck!")
@@ -101,7 +142,7 @@ function go(str)
     end
     if string.lower(str) == "up" or string.lower(str) == "u" then
         if turtle.up() then
-            current_pos["depth"] = current_pos["depth"] + 1
+            current_pos["y"] = current_pos["y"] + 1
             success = true
         else
             term.write("I'm stuck!")
@@ -149,61 +190,52 @@ function turn(str)
 end
 
 
-function goToPos(pos, order, canDig)
-   local isFinished = false
-   if order == nil then order = { "x", "z", "y" } end
-   if canDig == nil then canDig = false end
-
-   for f in order
-      if     f == string.lower("x") then moveX()
-      elseif f == string.lower("z") then moveZ()
-      else   f == string.lower("y") then moveY() end
-   end
-
-   local function moveX()
-      local xVector = [[]]
-
-   local function rotate(s,e)
-      local diff = s - e
-      local dir = {}
-      if (math.abs(dif) > 0) then
-         if     diff == -3 or diff ==  1 then dir = {1, "l"}
-         elseif diff ==  3 or diff == -1 then dir = {1, "r"}
-         else dir = {2, "r"}
-         for i = 0,dir[0] do
+function rotate(s,e)
+    local diff = s - e
+    local dir = {}
+    if (math.abs(diff) > 0) then
+        if     diff == -3 or diff ==  1 then dir = {1, "l"}
+        elseif diff ==  3 or diff == -1 then dir = {1, "r"}
+        else dir = {2, "r"} end
+        for i = 0,dir[0] do
             turn(dir[1])
-   end
-function copyPos(pos)
-   copy = { 1, 2, 3}
-   for k,v in current_pos do
-       
+        end
+    end
 end
 
 
+function goToPos(pos)
 
+    local s_dir = current_pos[4]
+    local e_dir = pos[4]
+    local copyPos = current_pos
 
+    if copyPos[2] < pos[2] then
+        for i in copyPos[2] do
+            go(u)
+        end
+        rotate(s_dir,3)
+        for i in copyPos[1] do
+            go(f)
+        end
+        rotate(3,2)
+        for i in copyPos[3] do
+            go(f)
+        end 
+        rotate(2,e_dir)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    elseif copyPos[2] > pos[2] then
+        rotate(s_dir,0)
+        for i in pos[1] do
+            go(f)
+        end
+        rotate(0,1)
+        for i in pos[3] do
+            go(f)
+        end 
+        rotate(0,e_dir)
+        for i in pos[2] do
+            go(d)
+        end
+    end
+end
